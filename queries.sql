78900ff1ad9668b8951c1ad35c25b3f3ca2af117
Miguel Batista <msb2@cesar.school>
2025-04-21 15:47:02 -0300

    CLEANUP ON USELESS FILES - RECOVER POINT

--- queries.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1743591236116@@127.0.0.1@3307@hospital_db
-USE hospital_db;
-
--- 1. Listar todos os pedidos realizados por um paciente específico
-SELECT p.ID_PEDIDO, p.ID_CAMAREIRA, p.DATA_PEDIDO, p.STATUS FROM PEDIDO p JOIN ESTADIA e ON p.ID_ESTADIA = e.ID_ESTADIA WHERE e.ID_ESTADIA = 1
-
-delete from FATURA where `ID_FATURA` = 1;
-
-delete from `ESTADIA` where `ID_ESTADIA` = 4
