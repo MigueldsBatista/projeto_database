ecae86b47e3705976af6cd808b4d54d0986a8b31
xTvini <vdas@cesar.school>
2025-05-26 18:14:53 -0300

    por questão de tempo e cansaço, achei melhor retirar todo o React e deixar a parte do paciente, no HTML antigo,

--- santajoana/src/main/java/com/hospital/santajoana/core/WebMvcConfiguration.java
+++ santajoana/src/main/java/com/hospital/santajoana/core/WebMvcConfiguration.java
@@ -14,7 +14,7 @@ public class WebMvcConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001","http://127.0.0.1:5500/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
